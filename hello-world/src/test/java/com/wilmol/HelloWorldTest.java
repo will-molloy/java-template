01ec69c71ae9b1f92f4254b5f6ba54a00b5217d0
Will Molloy <willjoemolloy@gmail.com>
2019-06-28 23:14:20 +1200

    use junit5

--- hello-world/src/test/java/com/wilmol/HelloWorldTest.java
+++ hello-world/src/test/java/com/wilmol/HelloWorldTest.java
@@ -2,17 +2,17 @@ package com.wilmol;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Example test src.
  *
  * @author wilmol
  */
-public class HelloWorldTest {
+class HelloWorldTest {
 
   @Test
-  public void test() {
+  void test() {
     assertThat(new HelloWorld().helloWorld()).isEqualTo("Hello world!");
   }
 }
