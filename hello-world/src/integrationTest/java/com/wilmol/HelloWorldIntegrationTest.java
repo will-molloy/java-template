e3d5b9dca7ad54f4384dd608a72d4fd4c0f9a280
Will Molloy <willjoemolloy@gmail.com>
2022-03-08 23:54:18 +1300

    Add example integration test

--- hello-world/src/integrationTest/java/com/wilmol/HelloWorldIntegrationTest.java
+++ hello-world/src/integrationTest/java/com/wilmol/HelloWorldIntegrationTest.java
@@ -5,11 +5,11 @@ import static com.google.common.truth.Truth.assertThat;
 import org.junit.jupiter.api.Test;
 
 /**
- * Example unit test.
+ * Example integration test.
  *
  * @author <a href=https://wilmol.com>Will Molloy</a>
  */
-class HelloWorldTest {
+class HelloWorldIntegrationTest {
 
   @Test
   void test() {
